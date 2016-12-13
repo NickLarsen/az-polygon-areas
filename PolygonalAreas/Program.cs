479a96c804d0b504782543bcf808a10c3c97ed22
Nick Larsen <fodylarsen@gmail.com>
2016-12-12 20:50:56 -0500

    what i have locally

---
+++ PolygonalAreas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonalAreas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
